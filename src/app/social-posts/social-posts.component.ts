4e8a5a87380ffed96a3a50c6df47f6f8206f1600
Nyki <nykilundgren127@gmail.com>
2020-01-29 16:21:03 -0500

    code

--- src/app/social-posts/social-posts.component.ts
+++ src/app/social-posts/social-posts.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../Models/Post';
 
 @Component({
   selector: 'app-social-posts',
   templateUrl: './social-posts.component.html',
   styleUrls: ['./social-posts.component.css']
 })
+
 export class SocialPostsComponent implements OnInit {
+  posts: Post[];
 
   constructor() { }
 
   ngOnInit() {
+    
+    this.posts = [
+      title: 'Grand Circus',
+      thought: 'Grand Circus is cool.'
+    ],
+    [
+      title: 'Ritual',
+      thought: 'Ritual is a well developped app.'
+    ],
+    [
+      title: 'Scott',
+      thought: 'Scott rides scooters'
+    ]
+
   }
+
 
 }
