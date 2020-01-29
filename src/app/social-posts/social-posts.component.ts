caf1c9de3c90c68f796066bea746d87b09e4183c
Nyki <nykilundgren127@gmail.com>
2020-01-29 16:29:17 -0500

    stupid typos

--- src/app/social-posts/social-posts.component.ts
+++ src/app/social-posts/social-posts.component.ts
@@ -8,24 +8,26 @@ import { Post } from '../Models/Post';
 })
 
 export class SocialPostsComponent implements OnInit {
+  
   posts: Post[];
-
   constructor() { }
 
   ngOnInit() {
     
     this.posts = [
-      title: 'Grand Circus',
+      {
+        title: 'Grand Circus',
       thought: 'Grand Circus is cool.'
-    ],
-    [
+      },
+      {
       title: 'Ritual',
       thought: 'Ritual is a well developped app.'
-    ],
-    [
+      },
+      {
       title: 'Scott',
       thought: 'Scott rides scooters'
-    ]
+      }
+      ]
 
   }
 
